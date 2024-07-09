0d4e919f40cac41a3bb3115d767f0e0d092903d3
NovaNexus7 <149381557+NovaNexus7@users.noreply.github.com>
2024-07-09 22:16:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeButton = document.querySelector('.theme-button');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navUl = document.querySelector('nav ul');
+    const currentTheme = localStorage.getItem('theme') || 'dark';
+
+    // Set the current theme on page load
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    // Toggle theme on button click
+    themeButton.addEventListener('click', () => {
+        const newTheme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+        document.documentElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+    });
+
+    // Toggle menu on hamburger click
+    hamburgerMenu.addEventListener('click', () => {
+        navUl.classList.toggle('show-menu');
+        hamburgerMenu.classList.toggle('nav-open');
+    });
+});
+
+
